6be168ef73d02e6a70b18d3cb80ea18a27f4de74
Dijoyce <celton.jordan@outlook.fr>
2024-10-16 11:19:43 +0200

    add form & css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,31 @@
 "use strict";
 
-// element toggle function
+// Fonction pour basculer la classe active d'un élément
 const elementToggleFunc = function (elem) {
   elem.classList.toggle("active");
 };
 
-// sidebar variables
+// Variables de la barre latérale
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
+// Fonctionnalité de bascule de la barre latérale pour mobile
 sidebarBtn.addEventListener("click", function () {
   elementToggleFunc(sidebar);
 });
 
-// custom select variables
+// Variables de la sélection personnalisée
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
+// Ouverture et fermeture de la liste déroulante
 select.addEventListener("click", function () {
   elementToggleFunc(this);
 });
 
-// add event in all select items
+// Ajout d'un événement à chaque élément de la liste déroulante
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
     let selectedValue = this.innerText.toLowerCase();
@@ -34,9 +35,10 @@ for (let i = 0; i < selectItems.length; i++) {
   });
 }
 
-// filter variables
+// Variables des éléments à filtrer
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
+// Fonction de filtrage des éléments
 const filterFunc = function (selectedValue) {
   for (let i = 0; i < filterItems.length; i++) {
     if (selectedValue === "all") {
@@ -49,7 +51,7 @@ const filterFunc = function (selectedValue) {
   }
 };
 
-// add event in all filter button items for large screen
+// Ajout d'événements à tous les boutons de filtre pour les grands écrans
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
@@ -64,16 +66,16 @@ for (let i = 0; i < filterBtn.length; i++) {
   });
 }
 
-// contact form variables
-const form = document.querySelector("[data-form]");
+// Variables du formulaire de contact
+const contactForm = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
+// Ajout d'un événement sur chaque champ de saisie du formulaire
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
-    // check form validation
-    if (form.checkValidity()) {
+    // Vérification de la validation du formulaire
+    if (contactForm.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
@@ -81,11 +83,11 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
-// page navigation variables
+// Variables de navigation de la page
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
+// Ajout d'événements à chaque lien de navigation
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
     for (let i = 0; i < pages.length; i++) {
@@ -101,36 +103,28 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-// Récupération des éléments de la modale
+// Variables pour la gestion de la modale
 const modal = document.getElementById("projectModal");
 const modalCloseBtn = document.querySelector(".close");
 const modalTitle = document.getElementById("modalTitle");
 const modalDescription = document.getElementById("modalDescription");
 
-// Récupération de tous les éléments de l'icône
+// Récupération de tous les éléments d'icônes de projet
 const iconBoxes = document.querySelectorAll(".project-item-icon-box");
 
-// Écouteurs d'événements pour chaque icône de projet
+// Ajout d'événements à chaque icône de projet pour afficher la modale
 iconBoxes.forEach((iconBox, index) => {
   iconBox.addEventListener("click", function () {
-    // Mettez à jour le contenu de la modale en fonction du projet cliqué
     const projectTitles = document.querySelectorAll(".project-title");
     const projectDescriptions = [
       "Création de la page d'accueil d'une agence de voyage avec une interface responsive utilisant HTML5 et CSS3. Intégration de maquettes Figma et développement de composants personnalisés pour une expérience utilisateur optimale.",
-
       "Objectifs d'apprentissage : conception mobile-first, implémentation d'animations CSS, utilisation de Sass pour un code maintenable, et versionnement avec Git et GitHub.",
-
       "Développement d'une page web dynamique pour un site d'architecte d'intérieur avec JavaScript. Interaction avec une API, création de pages dynamiques et utilisation d'outils comme Figma et Visual Studio Code.",
-
       "Implémentation du front-end d'une application avec React et React Router. Développement de composants, gestion de la navigation et utilisation de données simulées à partir d'un fichier JSON.",
-
       "Optimisation du référencement d'un site web en améliorant sa performance et son accessibilité. Analyse des problèmes, mise en œuvre d'améliorations et création d'un rapport détaillant les résultats avant/après.",
-
       "Débuggage et finalisation d'un site one-page. Analyse du code, résolution de bugs avec les Chrome DevTools et React Developer Tools, documentation des tests, et gestion de projet avec Yarn.",
-
       "Développement du front-end d'une application bancaire avec React et Redux. Intégration des appels API, création d'une interface utilisateur responsive et gestion de l'état global avec Redux.",
-
-      "Développement du projet **Menu Maker - Qwenta**, un outil en ligne pour la création et la personnalisation de menus par les restaurateurs. Le projet inclut la planification via un tableau Kanban et la mise en place d'une veille informatique pour rester informé des meilleures pratiques et technologies.",
+      "Développement du projet **Menu Maker - Qwenta**, un outil en ligne pour la création et la personnalisation de menus par les restaurateurs.",
     ];
 
     modalTitle.textContent = projectTitles[index].textContent;
@@ -145,7 +139,7 @@ modalCloseBtn.addEventListener("click", function () {
   modal.style.display = "none"; // Ferme la modale
 });
 
-// Fermeture de la modale si l'utilisateur clique à l'extérieur de la modale
+// Fermeture de la modale si l'utilisateur clique à l'extérieur
 window.addEventListener("click", function (event) {
   if (event.target === modal) {
     modal.style.display = "none"; // Ferme la modale
